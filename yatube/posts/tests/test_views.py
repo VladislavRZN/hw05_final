5acae19e8776c1130ad20b693a616e9dea5dd407
Владислав Буганов <vladislav-buganov@yandex.ru>
2023-04-02 19:22:10 +0300

    исправлено flace8

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -5,7 +5,7 @@ from django import forms
 from django.conf import settings
 from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import Client, TestCase, override_settings
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from ..models import Group, Post, User
